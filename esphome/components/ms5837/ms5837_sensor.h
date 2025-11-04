b1ea8c322d9ccb0a378829edb418a83bc2242127
stm91 <stm1991@hotmail.com>
2025-11-04 17:49:30 +0100

    Refactor MS5837 sensor header for driver integration
    Removed Arduino and Wire includes, added ms5837 driver include and memory header. Updated sensor initialization and replaced mode and osr members with a unique_ptr for the driver.

--- esphome/components/ms5837/ms5837_sensor.h
+++ esphome/components/ms5837/ms5837_sensor.h
@@ -1,8 +1,9 @@
+// esphome/components/ms5837/ms5837_sensor.h
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "Arduino.h"
-#include <Wire.h>
+#include "ms5837.h"              // your driver class & constants live here
+#include <memory>
 
 namespace esphome {
 namespace ms5837 {
@@ -13,18 +14,12 @@ class MS5837Sensor : public PollingComponent {
   sensor::Sensor *pressure_sensor = new sensor::Sensor();
   sensor::Sensor *altitude_sensor = new sensor::Sensor();
 
-  explicit MS5837Sensor(uint32_t update_ms = 60000,
-                        uint8_t mode = MS5837_MODE_RAW,
-                        uint8_t osr = MS5837_OSR_8192)
-      : PollingComponent(update_ms), mode_(mode), osr_(osr) {}
-
   void setup() override;
   void update() override;
   void dump_config() override;
 
  private:
-  uint8_t mode_;
-  uint8_t osr_;
+  std::unique_ptr<MS5837_Component> driver_;   // <-- needed by your .cpp
 };
 
 }  // namespace ms5837
